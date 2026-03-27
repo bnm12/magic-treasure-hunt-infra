39e979d4dfb8f296aaa05211a96775c3786acb3f
bnm12 <bnm12@users.noreply.github.com>
2026-03-27 22:17:50 +0100

    Vite ssl

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), basicSsl()],
   server: {
+    host: true,
+    https: true,
     port: 5173,
     watch: {
       // Watch src/ and public/ (hunt JSONs, images, sw.js, etc.)
